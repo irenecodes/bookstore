346c5c507500f5dec3d0692a80d6ccefcf5e9ec0
Irene Truong <irenesis123@gmail.com>
2019-08-26 03:05:11 -0400

    refactored each-book-comp to class component and added swal for delete button

--- src/components/each-book/each-book-component.jsx
+++ src/components/each-book/each-book-component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './each-book.scss';
 
 import {Link} from 'react-router-dom';
@@ -7,30 +7,63 @@ import { connect } from 'react-redux';
 
 import {deleteBook} from '../../redux/book/book-actions';
 
+import Swal from 'sweetalert2';
 
-const EachBook = ({book, deleteBook}) => (
-    <div className='each-book'>
-        <h4>Title: {book.title}</h4>
-        <p>Price in $: {book.price} </p>
-        <p>Category: {book.category}</p>
-        <p>Description: {book.description} </p>
-        
-        <Link to={{
-            pathname: '/editbook',
-            state: {
-                book: book
+
+
+class EachBook extends Component {
+
+    confirmDelete = e => {
+        const { book } = this.props;
+
+        Swal.fire({
+            title: 'Are you sure you want to delete?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your book has been deleted.',
+                    'success'
+                )
+                this.props.deleteBook(book)
             }
-        }}> Edit </Link>
+        })
+    }
 
 
-        <div
-            className='remove-button'
-            onClick={() => deleteBook(book)}>
-            Delete  &#10005;
+    render () {
+        const {book} = this.props;
+        return (
+            <div className='each-book'>
+                <h4>Title: {book.title}</h4>
+                <p>Price in $: {book.price} </p>
+                <p>Category: {book.category}</p>
+                <p>Description: {book.description} </p>
+
+                <Link to={{
+                    pathname: '/editbook',
+                    state: {
+                        book: book
+                    }
+                }}> Edit </Link>
+
+
+                <div
+                    className='remove-button'
+                    onClick={this.confirmDelete}
+                    >
+                    Delete  &#10005;
         </div>
 
-    </div>
-)
+            </div>
+        )
+    }
+}
 
 
 const mapDispatchToProps = dispatch => ({
