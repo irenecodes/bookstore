9e359b729b2c7d3a533ee5836fcd5d9fc699550a
Irene Truong <irenesis123@gmail.com>
2019-08-26 03:39:40 -0400

    Updates

--- precache-manifest.cd6cff1868471e3c19004709280ef49a.js
+++ precache-manifest.cd6cff1868471e3c19004709280ef49a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8fba8680ceb214bb0aa57c40900d67b0",
+    "revision": "cb2e3cda756e93444e9cfc1797f9a09e",
     "url": "/bookstore/index.html"
   },
   {
-    "revision": "762684fa6ace7a342918",
-    "url": "/bookstore/static/css/main.4a405ab2.chunk.css"
+    "revision": "d77c4da1d9597f4c1da4",
+    "url": "/bookstore/static/css/main.daf0473c.chunk.css"
   },
   {
     "revision": "42f8111dee049e9f08dc",
     "url": "/bookstore/static/js/2.3163987d.chunk.js"
   },
   {
-    "revision": "762684fa6ace7a342918",
-    "url": "/bookstore/static/js/main.6c3d3ae4.chunk.js"
+    "revision": "d77c4da1d9597f4c1da4",
+    "url": "/bookstore/static/js/main.9062c73f.chunk.js"
   },
   {
     "revision": "99016d41b3a535986def",
